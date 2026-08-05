d3a116af38b60c4b82d85ebe5bdbf6644807ec03
Diwakar Kapil <007.diwakarkapil@gmail.com>
2026-08-05 22:38:47 +0530

    Add Loops folder

---
+++ Loops.cpp/AllOddNumbersDivisibleBy3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=3;i<=100;i=i+6){
+        cout<<i<<" ";
+    }
+}
---
+++ Loops.cpp/GoodMorning.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<"Good Morning!"<<endl;
+    }
+}
+// for(int i = a ; i <= b ; i++)  this loop will run for (b-a+1) times.
+// a initialization, b condition, i increment/decrement
